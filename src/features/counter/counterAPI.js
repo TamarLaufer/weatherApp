cbd7ba4e244685b3e14f6681db4c139c1bfc05f7
Tamar Laufer <40568358+TamarLaufer@users.noreply.github.com>
2022-05-01 21:23:51 +0300

    api key

--- src/features/counter/counterAPI.js
+++ src/features/counter/counterAPI.js
@@ -1,23 +1,18 @@
 import urls from "../urls";
 
-const apikey = "boEG6HPhCrwgQvtZ9RspSdAgZHb9gKk4";
+const apikey = "xGrDp4bcnmA9r84G3H5DPgUUKxZ6IBVw";
 
 const fetchApi = async (url) => {
   let params = {
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    },
+    method: "GET",
   };
-
-  params.method = "GET";
   console.log("fetch url", url);
   return await fetch(url, params);
 };
 
 export const getCityData = async (city) => {
   return new Promise((resolve) => {
-    fetch(urls.city(apikey, city))
+    fetchApi(urls.city(apikey, city))
       .then((res) => res.json())
       .then((resJson) => {
         console.log("city res:", resJson);
@@ -32,7 +27,7 @@ export const getCityData = async (city) => {
 
 export const getFiveDaysForecasts = async (localKey) => {
   return new Promise((resolve) => {
-    fetch(urls.forecasts(apikey, localKey))
+    fetchApi(urls.forecasts(apikey, localKey))
       .then((res) => res.json())
       .then((resJson) => {
         console.log("getFiveDaysForecasts res:", resJson);
@@ -48,7 +43,7 @@ export const getFiveDaysForecasts = async (localKey) => {
 
 export const getAutocompleteCities = async (text) => {
   return new Promise((resolve) => {
-    fetch(urls.autocomplete(apikey, text))
+    fetchApi(urls.autocomplete(apikey, text))
       .then((res) => res.json())
       .then((resJson) => {
         resolve({ data: resJson });
