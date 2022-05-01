abd5d490d5054c6c894d1249d4a0ce9ab3381e9b
Tamar Laufer <40568358+TamarLaufer@users.noreply.github.com>
2022-05-01 21:35:14 +0300

    url

--- src/features/urls.js
+++ src/features/urls.js
@@ -1,8 +1,10 @@
-const mainUrl = "http://dataservice.accuweather.com/";
+const mainUrl = "https://dataservice.accuweather.com/";
 
 export default {
   city: (apikey, city) =>
     mainUrl + `locations/v1/cities/search?apikey=${apikey}&q=${city}`,
-    forecasts :(apikey, localkey)=> mainUrl + `forecasts/v1/daily/5day/${localkey}?apikey=${apikey}`,
-    autocomplete:(apikey, text)=>mainUrl + `locations/v1/cities/autocomplete?apikey=${apikey}&q=${text}`,
+  forecasts: (apikey, localkey) =>
+    mainUrl + `forecasts/v1/daily/5day/${localkey}?apikey=${apikey}`,
+  autocomplete: (apikey, text) =>
+    mainUrl + `locations/v1/cities/autocomplete?apikey=${apikey}&q=${text}`,
 };
